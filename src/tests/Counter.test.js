fac2d37b81f3d5a6c3bb4f5f097415b5696c6bf0
unknown <nivalditin89@gmail.com>
2021-08-01 13:47:40 -0400

    Counter unit test,test2 failed

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -6,8 +6,14 @@ import Counter from '../Counter';
 describe('Counter unit tests',()=>{
     let wrapper = shallow(<Counter/>);
 
-    test('must show correctly the component Counter',()=>{
+    test('test1,must show correctly the component Counter',()=>{
 
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('test2, should display the start count as 0',()=>{
+
+        const counterText = wrapper.find('h2').text().trim();
+        expect(counterText).toBe('0');
     })
 })
